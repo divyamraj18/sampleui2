f85be53102f92fa7992377778c96cc795fae957d
divyamraj18 <divyamraj18@gmail.com>
2020-05-22 07:40:47 +0530

    upadte 2

--- lib/listtileWidget.dart
+++ lib/listtileWidget.dart
@@ -24,60 +24,63 @@ class _ListTileWidgetState extends State<ListTileWidget> {
   @override
   Widget build(BuildContext context) {
     var deviceWidth=MediaQuery.of(context).size.width;
-    return  ListTile(
-      
-      title: Text('Lorem Ipsum Name'),
-      subtitle: Text('Lorem Ipsum Information Price'),
-      isThreeLine: true,
-      trailing: Container(
-        width:MediaQuery.of(context).size.width/2,
-        child: Row(
-          children: <Widget>[
-          Container(
-          height: 30,
-          width: 105,
-          decoration: BoxDecoration(
-            color: Colors.deepPurple,
-            borderRadius: BorderRadius.circular(5),
-            shape: BoxShape.rectangle,
-          ),
+    return  Container(
+      height: MediaQuery.of(context).size.height*.1,
+      child: ListTile(
+        title: Text('Lorem Ipsum Name'),
+        subtitle: Text('Lorem Ipsum Information Price'),
+        isThreeLine: true,
+        trailing: Container(
+          width:MediaQuery.of(context).size.width/2,
           child: Row(
             children: <Widget>[
-              IconButton(
-                icon: Icon(
-                  Icons.remove,
-                  color: Colors.white,
+            Container(
+            height: 30,
+            width: MediaQuery.of(context).size.width*.245,
+            decoration: BoxDecoration(
+              color: Colors.deepPurple,
+              borderRadius: BorderRadius.circular(5),
+              shape: BoxShape.rectangle,
+            ),
+            child: Row(
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(
+                    Icons.remove,
+                    color: Colors.white,
+                  ),
+                  onPressed: _decrement,
                 ),
-                onPressed: _decrement,
+                Container(
+                  child: Center(
+                    child: Text(count.toString(),style: TextStyle(color: Colors.white),),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.add,
+                    color: Colors.white,
+                  ),
+                  onPressed: _increment,
+                ),
+            ],
+            ),
+            ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width*0.1,
               ),
               Container(
-                child: Center(
-                  child: Text(count.toString(),style: TextStyle(color: Colors.white),),
+                width:  MediaQuery.of(context).size.width*0.15,
+                child: Text(
+                  '\$12.00',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20
+                  ),
                 ),
-              ),
-              IconButton(
-                icon: Icon(
-                  Icons.add,
-                  color: Colors.white,
-                ),
-                onPressed: _increment,
-              ),
-          ],
+              )
+            ],
           ),
-          ),
-            SizedBox(
-              width: 27,
-            ),
-            Container(
-              child: Text(
-                '\$12.00',
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 30
-                ),
-              ),
-            )
-          ],
         ),
       ),
     );
